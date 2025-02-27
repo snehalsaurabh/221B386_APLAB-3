a12297c27cc80e7185f01ecce72ec0e5bb78b736
Snehal Saurabh <snehalsaurabh.code@gmail.com>
2025-02-27 11:31:39 +0530

    Lab 4

--- Lab 4/Eager/EagerSingleton.java
+++ Lab 4/Eager/EagerSingleton.java
@@ -1,8 +1,6 @@
 public class EagerSingleton {
     private static final EagerSingleton instance = new EagerSingleton();
-
-    private EagerSingleton() {
-        
+    private EagerSingleton() {   
         System.out.println("EagerSingleton instance created.");
     }
     public static EagerSingleton getInstance() {
